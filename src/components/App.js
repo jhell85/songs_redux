8ce5740309b1958f384bb2d1e02bded9d2cd66bd
jhell85 <jhell85@gmail.com>
2020-09-15 08:26:51 -0700

    add reducers for songs

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { selectSong } from '../actions'
 const App = () => {
   return <div>App</div>
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+  return [
+    { title: "Beautiful Day", duration: "4:05" },
+    { title: "Kryptonite", duration: "3:15" },
+    { title: "Yellow", duration: "4:23" },
+    { title: "Ms. Jackson", duration: "2:36" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
